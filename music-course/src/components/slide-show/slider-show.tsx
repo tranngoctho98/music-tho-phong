1a2127a5a121c5bbf3796cb91a3a191293f237ac
Ngoc Tho Tran <tran1998tho@gmail.com>
2024-02-17 22:12:25 +0700

    add all

--- music-course/src/components/slide-show/slider-show.tsx
+++ music-course/src/components/slide-show/slider-show.tsx
@@ -2,6 +2,7 @@ import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import styled from "styled-components";
 import listImage from "../../assets/data-jsons/home/home-image.json";
+import { Box } from "@mui/material";
 
 const proprietes = {
   duration: 5000,
@@ -17,9 +18,22 @@ const SlideShowComponent = () => {
       <Slide {...proprietes}>
         {listImage.imageSlideShow.map((img, index) => (
           <div key={index} className="each-slide">
-            <div>
+            <Box className="width50" display={{ xs: "none", md: "block" }}>
               <img src={img} alt={img} />
-            </div>
+              <img
+                src={
+                  listImage.imageSlideShow[
+                    index + 1 === listImage.imageSlideShow.length
+                      ? 0
+                      : index + 1
+                  ]
+                }
+                alt={img}
+              />
+            </Box>
+            <Box className="width100" display={{ xs: "block", md: "none" }}>
+              <img src={img} alt={img} />
+            </Box>
           </div>
         ))}
       </Slide>
@@ -33,10 +47,17 @@ const SlideShowStyled = styled.div`
     margin-inline: auto;
   }
 
-  &&.container-slide img {
-    max-height: 500px;
-    min-height: 200px;
-    width: 100%;
+  &&.container-slide {
+    .width50 img {
+      max-height: 500px;
+      min-height: 200px;
+      width: 50%;
+    }
+    .width100 img {
+      max-height: 500px;
+      min-height: 200px;
+      width: 100%;
+    }
   }
 `;
 
